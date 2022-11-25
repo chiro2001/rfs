cf5c03abcc055de219d28d3b592f44417d95aaf0
Chiro Liang <Chiro2001@163.com>
2022-11-26 05:19:38 +0800

    mem in stack: stack overflow

--- disk_driver/src/memory.rs
+++ disk_driver/src/memory.rs
@@ -3,11 +3,13 @@ use anyhow::Result;
 use crate::*;
 use std::mem::size_of;
 
-const MEM_DISK_SIZE: usize = 4 * 0x400 * 0x400;
-const IOC_REQ_DEVICE_SIZE: u32 = (((2 as u32) << (((0 + 8) + 8) + 14)) | (('A' as u32) << (0 + 8)) | ((0) << 0) | ((size_of::<u32>() as u32) << ((0 + 8) + 8)));
-const IOC_REQ_DEVICE_STATE: u32 = (((2 as u32) << (((0 + 8) + 8) + 14)) | (('A' as u32) << (0 + 8)) | ((1) << 0) | ((size_of::<u32>() as u32 * 3) << ((0 + 8) + 8)));
-const IOC_REQ_DEVICE_RESET: u32 = (((0 as u32) << (((0 + 8) + 8) + 14)) | (('A' as u32) << (0 + 8)) | ((2) << 0) | ((0) << ((0 + 8) + 8)));
-const IOC_REQ_DEVICE_IO_SZ: u32 = (((2 as u32) << (((0 + 8) + 8) + 14)) | (('A' as u32) << (0 + 8)) | ((3) << 0) | ((size_of::<u32>() as u32) << ((0 + 8) + 8)));
+// const MEM_DISK_SIZE: usize = 4 * 0x400 * 0x400;
+const MEM_DISK_SIZE: usize = 4 * 0x400 * 0x4;
+const MEM_DISK_UNIT: usize = 512;
+const IOC_REQ_DEVICE_SIZE: u32 = ((2 as u32) << (((0 + 8) + 8) + 14)) | (('A' as u32) << (0 + 8)) | ((0) << 0) | ((size_of::<u32>() as u32) << ((0 + 8) + 8));
+const IOC_REQ_DEVICE_STATE: u32 = ((2 as u32) << (((0 + 8) + 8) + 14)) | (('A' as u32) << (0 + 8)) | ((1) << 0) | ((size_of::<u32>() as u32 * 3) << ((0 + 8) + 8));
+const IOC_REQ_DEVICE_RESET: u32 = ((0 as u32) << (((0 + 8) + 8) + 14)) | (('A' as u32) << (0 + 8)) | ((2) << 0) | ((0) << ((0 + 8) + 8));
+const IOC_REQ_DEVICE_IO_SZ: u32 = ((2 as u32) << (((0 + 8) + 8) + 14)) | (('A' as u32) << (0 + 8)) | ((3) << 0) | ((size_of::<u32>() as u32) << ((0 + 8) + 8));
 
 pub struct MemoryDiskDriver {
     pub info: DiskInfo,
@@ -17,6 +19,7 @@ pub struct MemoryDiskDriver {
 
 impl DiskDriver for MemoryDiskDriver {
     fn ddriver_open(self: &mut Self, path: &str) -> Result<()> {
+        println!("MemDrv open: {}", path);
         Ok(())
     }
 
@@ -35,15 +38,15 @@ impl DiskDriver for MemoryDiskDriver {
 
     fn ddriver_write(self: &mut Self, buf: &[u8], size: usize) -> Result<usize> {
         assert!(buf.len() >= size);
-        self.get_pointer_slice().copy_from_slice(&buf[..size]);
+        self.get_pointer_slice(size).copy_from_slice(&buf[..size]);
         self.pointer += size;
-        Ok((size))
+        Ok(size)
     }
 
     fn ddriver_read(self: &mut Self, buf: &mut [u8], size: usize) -> Result<usize> {
-        buf[..size].copy_from_slice(self.get_pointer_slice());
+        buf[..size].copy_from_slice(self.get_pointer_slice(size));
         self.pointer += size;
-        Ok((size))
+        Ok(size)
     }
 
     fn ddriver_ioctl(self: &mut Self, cmd: u32, arg: &mut [u8]) -> Result<()> {
@@ -62,7 +65,10 @@ impl DiskDriver for MemoryDiskDriver {
             IOC_REQ_DEVICE_RESET => {
                 self.ddriver_reset()
             }
-            IOC_REQ_DEVICE_IO_SZ => todo!(),
+            IOC_REQ_DEVICE_IO_SZ => {
+                arg[0..4].copy_from_slice(&self.info.consts.iounit_size.to_le_bytes());
+                Ok(())
+            }
             _ => Ok(())
         }
     }
@@ -83,6 +89,7 @@ impl MemoryDiskDriver {
                 stats: Default::default(),
                 consts: DiskConst {
                     layout_size: MEM_DISK_SIZE as u64,
+                    iounit_size: MEM_DISK_UNIT as u32,
                     ..Default::default()
                 },
             },
@@ -91,8 +98,8 @@ impl MemoryDiskDriver {
         }
     }
 
-    fn get_pointer_slice(self: &mut Self) -> &mut [u8] {
-        &mut self.mem[self.pointer..]
+    fn get_pointer_slice(self: &mut Self, size: usize) -> &mut [u8] {
+        &mut self.mem[self.pointer..(size + self.pointer)]
     }
 }
 
@@ -102,7 +109,14 @@ mod tests {
     use anyhow::Result;
 
     fn driver_tester(driver: &mut dyn DiskDriver) -> Result<()> {
-        driver.ddriver_open("test");
+        driver.ddriver_open("test")?;
+        let write_data = [0x55 as u8; MEM_DISK_UNIT];
+        driver.ddriver_write(&write_data, MEM_DISK_UNIT)?;
+        driver.ddriver_seek(0, SeekType::Set)?;
+        let mut read_data = [0 as u8; MEM_DISK_UNIT];
+        driver.ddriver_read(&mut read_data, MEM_DISK_UNIT)?;
+        assert_eq!(read_data, write_data);
+        driver.ddriver_close()?;
         Ok(())
     }
 
