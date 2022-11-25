8da184749e34eb20e74c06770fedefc4fdab768d
Chiro Liang <Chiro2001@163.com>
2022-11-26 01:49:39 +0800

    DiskDriver trait

--- disk_driver/src/lib.rs
+++ disk_driver/src/lib.rs
@@ -1,14 +1,13 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub trait DiskDriver {
+    fn ddriver_open(path: &str) -> i32;
+    fn ddriver_close(fd: i32) -> i32;
+    fn ddriver_seek(fd: i32, offset: i64, whence: i32) -> i32;
+    fn ddriver_write(fd: i32, buf: &[u8], size: usize) -> i32;
+    fn ddriver_read(fd: i32, buf: &[u8], size: usize) -> i32;
+    fn ddriver_ioctl(fd: i32, cmd: u32, arg: &[u8]) -> i32;
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
